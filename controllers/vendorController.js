e922c22ac1d82ccb9c025c98904a32d4861c4876
MaheshGuptak1 <maheshguptak8@gmail.com>
2024-08-16 14:16:43 +0530

    a

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -1,5 +1,5 @@
 const Vendor = require('../models/Vendor');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 const Firm = require('../models/Firm');
